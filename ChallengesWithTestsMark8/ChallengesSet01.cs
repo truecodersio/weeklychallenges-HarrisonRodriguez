b950a15deacf369f6774cfff62e27849da835a38
Harrison Rodriguez <harrisonmrod@gmail.com>
2019-09-10 14:08:40 -0500

    Finished Week1 Challenge

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,56 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double x = minuend;
+            double y = subtrahend;
+
+            return x - y;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int x = number1;
+            int y = number2;
+
+            return x + y;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if(number1 > number2)
+            {
+                return number2;
+
+            }
+            else
+            {
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long x = factor1;
+            long y = factor2;
+
+            return x * y;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return "Hello, " + nameOfPerson + "!";
+            }
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
